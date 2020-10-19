e7ce8186cf3eb24e2eae8a9164d7dd631ee09913
jerrygerou <jerrysgerou@gmail.com>
2020-10-19 17:42:18 -0500

    Component creation

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import Header from "./components/Header";
+import Inventory from "./components/Inventory";
+import Order from "./components/Order";
+import "./css/style.css";
+
+render(<App />, document.querySelector("#main"));
