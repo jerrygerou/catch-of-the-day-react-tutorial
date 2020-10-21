4eed5c2e895262222d9807bdef0c344924c81ac0
jerrygerou <jerrysgerou@gmail.com>
2020-10-20 21:03:47 -0500

    Create fish object, set state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,17 +4,33 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
-render() {
-    return (
-        <div className="catch-of-the-day">
-            <div className="menu">
-                <Header tagline="Fresh Seafood Market" />
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = fish => {
+        // Take a copy of existing state
+        const fishes = { ...this.state.fishes };
+        // Add new fish to fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // Set new fishes object to state
+        this.setState({
+            fishes: fishes
+        })
+    }
+
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market" />
+                </div>
+                <Order />
+                <Inventory addFish={this.addFish} />
             </div>
-            <Order />
-            <Inventory />
-        </div>
-    )
-}
+        )
+    }
 }
 
 export default App;
