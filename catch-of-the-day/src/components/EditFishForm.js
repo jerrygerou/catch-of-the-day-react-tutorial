32d3410bea4737d765821d862ed747034897f018
jerrygerou <jerrysgerou@gmail.com>
2020-10-24 14:46:19 -0500

    Add validation for each propType

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -1,6 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class EditFishForm extends React.Component {
+    static propTypes = {
+        fish: PropTypes.shape({
+            image: PropTypes.string,
+            name: PropTypes.string,
+            desc: PropTypes.string,
+            status: PropTypes.string,
+            price: PropTypes.oneOfType([
+                PropTypes.string,
+                PropTypes.number
+            ])
+        }),
+        index: PropTypes.string,
+        updateFish: PropTypes.func
+    }
     handleChange = (event) => {
         // update that fish
         // take a copy of current fish
@@ -17,7 +32,7 @@ class EditFishForm extends React.Component {
         return (
             <div className="fish-edit">
                 <input type="text" name="name" onChange={this.handleChange} value={this.props.fish.name} />
-                <input type="text" name="price" onChange={this.handleChange} value={this.props.fish.price} />
+                <input type="number" name="price" pattern="[0-9]*" onChange={this.handleChange} value={this.props.fish.price} />
                 <select name="status" onChange={this.handleChange} value={this.props.fish.status}>
                     <option value="available">Fresh!</option>
                     <option value="unavailable">Sold Out!</option>
